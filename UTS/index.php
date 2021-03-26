62a85858e76c6b9720ad41c924331b5795e202f6
MuhammadSatrioFarhan <80187916+MuhammadSatrioFarhan@users.noreply.github.com>
2021-03-26 22:46:37 +0700

    Add files via upload

---
+++ UTS/index.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+<title>EDU-COVID</title>
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<div class="wrapper row0">
+  <header id="header" class="hoc clear"> 
+    <div id="logo" class="one_quarter first">
+      <h1 class="logoname clear"><a href="index.html"><span>EDU-COVID</span></a></h1>
+    </div>
+    <div class="three_quarter">
+      <ul class="nospace clear">
+        <li class="one_third first">
+          <div></div>
+        </li>
+        <li class="one_third">
+          <div></div>
+        </li>
+        <li class="one_third">
+          <div class="block clear"><a href="tel:119"><i class="fas fa-phone"></i> </a><span><strong>Covid-19 Hotline :</strong></span> 119 </div>
+        </li>
+      </ul>
+    </div>
+  </header>
+</div>
+<div class="wrapper row1">
+  <div class="hoc1 clear">
+    <nav id="mainav">
+      <ul class="clear">
+        <li class="navbar"><a class="b" href="index.html">Home</a></li>
+        <li class="navbar"><a class="b" href="pencegahancovid.html">Cara Pencegahan</a></li>
+        <li class="navbar"><a class="b" href="statistik.html">Statistic</a></li>
+        <li class="navbar"><a class="b" href="kuis.html">Kuis</a></li>
+        <li class="navbar"><a class="b" href="kontak.html">Kontak</a></li>
+      </ul>
+    </nav>
+  </div>
+</div>
+<div class="wrapper row2">
+  <section class="hoc container clear"> 
+    <ul class="nospace group latest">
+      <li class="one_third first">
+      </li>
+      <li class="one_third">
+      </li>
+      <li class="one_third">
+      </li>
+    </ul>
+  </section>
+</div>
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <article class="group">
+      <div class="one_half first">
+             <h1 class="logoname clear"><a href="index.html"><span>EDU-COVID</span></a></h1>
+        <p>EDU-COVID adalah sebuah website yang akan menampilkan informasi - informasi yang berkaitan dengan CORONA VIRUS , anda juga dapat melihat cara -cara pencegahan, statistik , kuis yang dimana seberapa tahu anda dengan COVID-19 , dan terdapat kontak supaya anda bisa berkomunikasi atau menanyakan suatu hal COVID-19 yang anda belum mengetahuinya ke pengelola EDU-COVID </p>
+        </div>
+         <div class="one_half"><a class="imgover" href="#"><img class="borderedbox inspace-30 " src="images/cor.jpg" alt=""></a></div>
+    </article>
+    <div class="clear"></div>
+  </main>
+</div>
+<div class="wrapper row2">
+  <section class="hoc container clear"> 
+    <ul class="nospace group latest">
+      <li class="one_third first">
+      </li>
+      <li class="one_third">
+      </li>
+      <li class="one_third">
+      </li>
+    </ul>
+  </section>
+</div>
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <p class="fl_left">Copyright &copy; 2021 - Muhammad Satrio Farhan</p>
+  </div>
+</div>
+<a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
